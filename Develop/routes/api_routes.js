5316f891200f71daf842698bcc21f2ed185a1c0c
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-17 23:02:50 -0400

    added get, post and delete routes to api_routes.js

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -1,6 +1,26 @@
 const fs = require('fs');
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {notes} = require('../db/db.json');
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, '../public/index.html'));
+router.get('/notes', (req, res) => {
+    let data = notes;
+    res.json(data);
+});
+
+
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
+    let newNote = renderActiveNote(req.body, notes);
+    res.json(newNote);
+});
+
+
+
+router.delete('/notes/:id', (req, res) => {
+   const noteParam = req.params.id;
+   renderNoteList(params, notes);
+   res.redirect('');
 });
