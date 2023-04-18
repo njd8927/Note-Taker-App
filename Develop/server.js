cd2070dc1d5a6cb5b4f0c0436de729191ce89110
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-17 22:00:00 -0400

    set up server in server.js file

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+// import required packages and files
+const express = require ('express');
+const apiRoutes = require ('./routes/api_routes.js');
+const htmlRoutes = require ('./routes/html_routes.js');
+
+// Initialize app and create port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Use methods for parsing and middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static ('public'));
+// app.use('/api', apiRoutes);
+// app.use('/api', htmlRoutes);
+
+
+
+
+app.listen(PORT, () => 
+    console.log('Server listening on Port %s', PORT));
