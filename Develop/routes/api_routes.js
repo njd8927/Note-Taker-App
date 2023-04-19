7775ef60be7c0a92979d84cbbf3f790b961b9a42
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-18 21:06:58 -0400

    added pseudocode to routes files

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -1,9 +1,10 @@
+// import required packages
 const fs = require('fs');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 let notes  = require('../db/db.json');
 
-
+// set up get endpoint to get notes data, log it and get the notes array back as JSON
 router.get('/notes', (req, res) => {
     let data = notes;
     console.log(data);
@@ -11,7 +12,8 @@ router.get('/notes', (req, res) => {
 });
 
 
-
+// set up post endpoint and add a new note to the existing notes array
+// give new note a unique ID and get the notes array back as JSON
 router.post('/notes', (req, res) => {
     req.body.id = uuidv4();
     notes.push(req.body)
@@ -19,7 +21,8 @@ router.post('/notes', (req, res) => {
 });
 
 
-
+// set up delete endpoint and delete a note from notes array with the matching ID
+// redirect back to home page when note deleted
 router.delete('/notes/:id', (req, res) => {
    const noteParam = req.params.id;
    const newNotes = notes.filter(note => req.params.id !== note.id)
@@ -28,4 +31,5 @@ router.delete('/notes/:id', (req, res) => {
 });
 
 
+// export router
 module.exports = router;
--- Develop/routes/html_routes.js
+++ Develop/routes/html_routes.js
@@ -1,19 +1,22 @@
+// import required packages
 const path = require('path');
 const router = require('express').Router();
 
+
+// set up get endpoint and send back index.html to user if they type the root URL
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-
+// set up get endpoint and send back notes.html to user if they type the root URL/notes
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-
+// set up get endpoint and send back index.html to user if they type the root URL/*
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-
+// export router
 module.exports = router;
